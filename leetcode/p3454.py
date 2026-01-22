fef471ae58e558aff93dc6d9bc9f34c179da2e69
bigface008 <zhwang98@outlook.com>
2026-01-21 20:05:34 -0800

    Backup.

---
+++ leetcode/p3454.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/separate-squares-ii/description/?envType=daily-question&envId=2026-01-14
+from typing import List, Optional, Dict
+
+
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        pass

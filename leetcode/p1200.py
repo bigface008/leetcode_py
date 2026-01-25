bbb8c851e41e71a5444a4cdefd3b21cb94046fde
bigface008 <zhwang98@outlook.com>
2026-01-25 11:19:34 -0800

    Backup.

---
+++ leetcode/p1200.py
@@ -0,0 +1,22 @@
+# https://leetcode.cn/problems/minimum-absolute-difference/?envType=daily-question&envId=2026-01-26
+from collections import defaultdict
+from typing import List, Dict, Tuple
+from math import inf
+
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        N = len(arr)
+        min_diff = inf
+        min_diff_pairs: List[List[int]] = []
+        diff_to_pairs: Dict[int, List[Tuple[int, int]]] = defaultdict(list)
+        for i in range(N - 1):
+            a, b = arr[i], arr[i + 1]
+            diff = b - a
+            if diff < min_diff:
+                min_diff_pairs = [[a, b]]
+                min_diff = diff
+            elif diff == min_diff:
+                min_diff_pairs.append([a, b])
+        return min_diff_pairs

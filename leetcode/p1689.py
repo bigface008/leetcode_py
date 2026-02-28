b716a3a1580b2835638f270518f6b6e5dec425a8
bigface008 <zhwang98@outlook.com>
2026-02-28 15:33:08 -0800

    Backup.

---
+++ leetcode/p1689.py
@@ -0,0 +1,7 @@
+# https://leetcode.cn/problems/partitioning-into-minimum-number-of-deci-binary-numbers/?envType=daily-question&envId=2026-03-01
+from typing import List, Dict, Tuple
+
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
